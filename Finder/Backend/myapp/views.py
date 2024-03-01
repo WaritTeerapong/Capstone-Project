2213adb6a7d84fa12325789bf87f1ed38e654d83
WaritTeerapong <warit.teer@kmutt.ac.th>
2024-03-01 13:59:03 +0700

    try to direct path in myapp

--- Finder/Backend/myapp/views.py
+++ Finder/Backend/myapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Finder.")
+    return HttpResponse(" <h1>Search for love -> Tinder.<br> Search for lost -> Finder.</h1>")
+
+def about(request):
+    return HttpResponse(" Get to know us more")
